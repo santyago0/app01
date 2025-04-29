de98b05097c03c6d08102e231aae3d3daaad5518
santyago0 <santyago0515@gmail.com>
2025-04-29 09:54:20 -0500

    Tabla dinamica desde JSON

--- src/main.js
+++ src/main.js
@@ -2,5 +2,26 @@ import $ from 'jquery';
 import dt from 'datatables.net-dt';
 
 $(document).ready(function () {
-    $('#tabla-ejemplo').DataTable();
+    const mi_tabla = $('#tabla-ejemplo').DataTable();
+
+    var url = "https://pkgstore.datahub.io/core/continent-codes/continent-codes_json/data/60d6baef1250bc2b01fd0148dccca518/continent-codes_json.json";
+    $("#obtenerInformacion").click(function () {
+        $.ajax({
+            dataType: 'json',
+            url: url
+        }).then(function (data) {
+
+            data.forEach(function (item) {
+                mi_tabla.row.add([
+                    item.Code,
+                    item.Name
+                ]).draw(false);
+            });
+        });
+    });
+
+    $("#limpiarInformacion").click(function () {
+        mi_tabla.clear().draw();
+    });
+
 });
